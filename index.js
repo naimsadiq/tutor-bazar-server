7139fcb76fc3efd6b8c0fc33483ae1d4f8c64822
naimsadiq <nayemsadiq2013@gmail.com>
2025-12-16 06:32:34 +0600

    update code fix

--- index.js
+++ index.js
@@ -885,6 +885,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log("Server running on port " + port);
-});
+// app.listen(port, () => {
+//   console.log("Server running on port " + port);
+// });
